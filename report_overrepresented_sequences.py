66d0a77873d70af433e57b2b601fbe857beac391
Morgan Taschuk <morgan.taschuk@oicr.on.ca>
2015-03-17 14:04:12 -0400

    Only report adapter contamination; reduce number of columns; summarize all adapter contamination on one line

--- report_overrepresented_sequences.py
+++ report_overrepresented_sequences.py
@@ -13,29 +13,50 @@ def open_fastqc(fastqc_zip,match_string,firstbit):
     """ 
     Open the fastqc zip, find the fastqc_data.txt and pull out the matches as specified in match_strings.
     """
-    annotations=False
+
     #open the fastqc zip
     try:
         match_string=">>Overrepresented sequences"
-
+        m=re.match(".*?_(R[1|2])_.*",fastqc_zip)
+        if m is not None:
+            firstbit="\t".join([firstbit, m.group(1)])
+        else:
+            firstbit=firstbit+"\t "
         with zipfile.ZipFile(fastqc_zip,'r') as myzip:
             matching = filter(lambda element: "fastqc_data.txt" in element, myzip.namelist())
             iterator=iter(myzip.read(matching[0],'rU').split("\n"))
-            for line in iterator:
-                if line.startswith(match_string):
-                    line = iterator.next()
-                    while not line.startswith(">"):
-                        if not line.startswith("#"):
-                            print(firstbit+"\t"+line)
-                        line = iterator.next()
-                    annotations=True
-                    break
-        if annotations==False:
-            print(firstbit+"\tN/A")
+            doathing(iterator,match_string,firstbit)
     except zipfile.BadZipfile as e:
         print(" : ".join([time.strftime("%x %X"), "ERROR: BadZipfile", str(e), fastqc_zip]),file=sys.stderr)
     except IOError as e:
         print(" : ".join([time.strftime("%x %X"), "ERROR: IOError", str(e), fastqc_zip]),file=sys.stderr)
+
+def doathing(iterator,match_string,firstbit):
+    annotations=False    
+    for line in iterator:
+        if line.startswith(match_string):
+            line = iterator.next()
+            percent=0.0
+            source=[]
+            while not line.startswith(">"):
+                if not line.startswith("#"):
+                    report=line.split("\t")
+                    lineper=float(report[2])
+                    linesou=report[3]
+                    if "Adapter" in linesou:
+                        matstr="(.+?)[,|(]"
+                        m=re.match(matstr,linesou)
+                        if m is not None:
+                            linesou=m.group(1)
+                            percent=percent+lineper
+                            source.append(linesou)
+                line = iterator.next()
+            if source:
+                annotations=True
+                print("\t".join([firstbit, str(percent), ",".join(set(source))]))
+                break
+            #if annotations is not True:
+            #    print(firstbit+"\tN/A")
 
 def main(argv):
    reportarg = []
@@ -75,13 +96,13 @@ def main(argv):
 
    #open the file provenance report
    with open(sw_filename) as tsv:
-      print("\t".join(['Project', 'Identity', 'Library', 'Run', 'Lane', 'Barcode', 'Sequence', 'Count', 'Percentage', 'Possible Source']))
+      print("\t".join(['Project', 'Library', 'Run', 'Lane', 'Barcode', 'Sequence', 'Count', 'Percentage', 'Possible Source']))
        #parse the report into a map
       for line in csv.DictReader(tsv, delimiter="\t"):
          annos=None
 	 #find the FastQC workflow reports and pull out the annotation lines
          if line['Workflow Name'] == 'FastQC' and line['File Meta-Type'] == 'application/zip-report-bundle':
-            firstbit="\t".join([ line['Study Title'], line['Root Sample Name'], line['Sample Name'], line['Sequencer Run Name'], line['Lane Number'], line['IUS Tag'] ])
+            firstbit="\t".join([ line['Study Title'], line['Sample Name'], line['Sequencer Run Name'], line['Lane Number'], line['IUS Tag'] ])
             open_fastqc(line['File Path'],match_string,firstbit)
 
 
