e8d3fb865f5b0bd7aea3987408b0fa8f7b560d2e
aduncan <andrew.duncan@oicr.on.ca>
2015-05-27 16:14:15 -0400

    Adapter contamination script now works with gzip files

--- report_overrepresented_sequences.py
+++ report_overrepresented_sequences.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python
+#!/oicr/local/sw/Python-2.7.2/bin/python2.7
 from __future__ import print_function
 
 import sys,getopt,time
 import subprocess
-import csv,zipfile,re
+import csv,re
 import os
+import gzip
 
 def usage(long_opts):
    print('report_overrepresented_sequences.py',"[","--"+" --".join(long_opts).replace("="," <val>"),"]")
    print('   use -h to print this message')
 
 def main(argv):
-   reportarg = []
-   test=False
    sw_filename=None
    adapter_seq=None
    long_opts=["use-sw-file=", "adapter-sequence="]
@@ -29,15 +28,9 @@ def main(argv):
          sw_filename=arg
       elif opt == '--adapter-sequence':
 	adapter_seq=arg
-
-
-#   if sw_filename is None:
-        #generate the report
- #       sw_filename="tmp.tsv"
-  #      mycall = ["seqware", "files", "report", "--out", sw_filename];
-   #     mycall.extend(reportarg)
-    #    call(mycall)
-
+   if adapter_seq is None or sw_filename is None:
+      print("Make sure you give arguments for --use-sw-file and --adapter-sequence.")
+      sys.exit()
 
    # Make csv file to write to
    csvFile = open('AdapterContamination.csv','w')
@@ -45,13 +38,12 @@ def main(argv):
    csvFile.write(header + "\n")
 
    #open the file provenance report
-   with open(sw_filename) as tsv:
+   with gzip.open(sw_filename) as tsv:
        #parse the report into a map
       for line in csv.DictReader(tsv, delimiter="\t"):
          annos=None
 	 #find the FastQ files and calculates their adapter contamination numbers
          if 'chemical/seq-na-fastq-gzip' in line['File Meta-Type']:
-             print("in chemical")
              firstbit=",".join([ line['Study Title'], line['Sample Name'], line['Sequencer Run Name'], line['Lane Number'], line['IUS Tag'] ])
              # Get Trimmed reads percentage
              filePath = line['File Path'];
@@ -63,7 +55,7 @@ def main(argv):
              p1 = subprocess.Popen(["zcat", filePath], stdout=subprocess.PIPE)
              p2 = subprocess.Popen(["head", "-100000"], stdin=p1.stdout, stdout=subprocess.PIPE)
              p1.stdout.close()
-             p3 = subprocess.check_output("cutadapt -a %s -O 10 -o /dev/null -" % (adapter_seq), stderr=subprocess.STDOUT, stdin=p2.stdout, shell=True)
+             p3 = subprocess.check_output("/oicr/local/analysis/sw/cutadapt/cutadapt-0.9.3/cutadapt -a %s -O 10 -o /dev/null -" % (adapter_seq), stderr=subprocess.STDOUT, stdin=p2.stdout, shell=True)
              p2.stdout.close()
              m = re.findall('\(\s+\d+\.\d+%\)', p3)
              m2 = re.findall('\d+\.\d+%', str(m))
@@ -71,8 +63,6 @@ def main(argv):
              # Write to csv file
              line = firstbit + "," + m2[0]
              csvFile.write(line + "\n")
-         else:
-             print("not in chemical")
    csvFile.close()
 
 if __name__ == "__main__":
